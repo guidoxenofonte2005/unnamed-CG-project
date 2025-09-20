2455e45d58cf922b0df394c70394b3f8c2b2c302
Guido <119985142+guidoxenofonte2005@users.noreply.github.com>
2025-09-20 11:14:04 -0300

    adding keyboard/mouse functions for camera/player movement

--- unnamedCGproject/main.c
+++ unnamedCGproject/main.c
@@ -4,6 +4,12 @@
 #include <windows.h>
 #include <GL/freeglut.h>
 
+int verticalMovement;
+int horizontalMovement;
+
+int lastMousex, lastMousey;
+
+
 int init() {
     glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
     glEnable(GL_DEPTH_TEST);
@@ -26,18 +32,38 @@ void display() {
     glColor3f(1.0f, 0.0f, 0.0f);
     glutWireCube(1.0f);
 
-    glFlush();
+    glutSwapBuffers();
+}
+
+void handleKeyboardInput() {
+
+}
+
+void handleMouseMovement(int x, int y) {
+    int dx = x - lastMousex;
+    int dy = y - lastMousey;
+
+    float mouseSensitivity = 0.005f;
+
+    float thetaAngle = dx * mouseSensitivity;
+
 }
 
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
+
+    // mudar diretivas de inicialização depois pra algo melhor
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowPosition(200, 0);
     glutInitWindowSize(500, 500);
     glutCreateWindow("HALLO");
 
     init();
+
+    glutKeyboardFunc(handleKeyboardInput);
+    glutPassiveMotionFunc(handleMouseMovement); // função que detecta movimento do mouse de forma passiva (sem nenhum botão pressionado)
+
     glutDisplayFunc(display);
     glutMainLoop();
     return 0;

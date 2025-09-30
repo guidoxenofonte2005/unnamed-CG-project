b3a75bd8df47bfb0e6ac8a194e8589b9b786de8a
Guido <119985142+guidoxenofonte2005@users.noreply.github.com>
2025-09-30 13:37:37 -0300

    removed objects texture folder, added fileManager.h file

--- unnamedCGproject/fileManager.c
+++ unnamedCGproject/fileManager.c
@@ -3,13 +3,22 @@
 
 #include <string.h>
 
+#include "fileManager.h"
+
 FILE *loadFilePointer = NULL;
 
-void loadFile(char *fileLocation) {
+void loadObjectsFromFile(char *fileLocation) {
     if (strcmp(fileLocation, "")) return;
 
     loadFilePointer = fopen(fileLocation, "r");
     if (loadFilePointer == NULL) return;
 
+    char line[MAX_LINE_SIZE];
+    while (fgets(line, sizeof(line), loadFilePointer)) {
+        const char *delimiters = ";";
+        char *token = strtok(line, delimiters);
+        printf("%s, %s", token[0], token[1]);
+    }
 
+    fclose(loadFilePointer);
 }
---
+++ unnamedCGproject/fileManager.h
@@ -0,0 +1,8 @@
+#ifndef FILEMANAGER_H_INCLUDED
+#define FILEMANAGER_H_INCLUDED
+
+#define MAX_LINE_SIZE 256
+
+#endif // FILEMANAGER_H_INCLUDED
+
+void loadObjectsFromFile(char *fileLocation);
